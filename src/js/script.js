5fd9f85b24354d6c39ca34f6e6199e23a035016e
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2022-12-29 22:13:21 +0200

    previous and next slide navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,16 @@
 const slides = document.querySelectorAll('.slides > .slide');
+const next = document.querySelector('.navigation-btn > .next');
+const prev = document.querySelector('.navigation-btn > .prev');
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
 
 const timeout = 7000;
 
 let slide = startFromSlide();
+let timeoutId;
 
 start();
-
-function start() {
-    setTimeout(run, timeout);
-}
 
 function run() {
     slider();
@@ -16,8 +18,25 @@ function run() {
 }
 
 function slider() {
+    animationOn();
     increment();
     changeSlide();
+}
+
+function nextSlide() {
+    stop();
+    animationOff();
+    increment();
+    changeSlide();
+    start();
+}
+
+function prevSlide() {
+    stop();
+    animationOff();
+    decrement();
+    changeSlide();
+    start();
 }
 
 function changeSlide() {
@@ -30,10 +49,39 @@ function changeSlide() {
     }
 }
 
+function start() {
+    timeoutId = setTimeout(run, timeout);
+}
+
+function stop() {
+    clearTimeout(timeoutId);
+}
+
+function animationOff() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.add('na');
+    }
+}
+
+function animationOn() {
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].classList.contains('na')) {
+            slides[i].classList.remove('na');
+        }
+    }
+}
+
 function increment() {
     slide++;
     if (slide > slides.length - 1) {
         slide = 0;
+    }
+}
+
+function decrement() {
+    slide--;
+    if (slide < 0) {
+        slide = slides.length - 1;
     }
 }
 
