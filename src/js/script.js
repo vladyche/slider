e467e5fc3e68f9af13f42583468e34d833badcfb
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2022-12-29 19:36:03 +0200

    change slides recursively

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,46 @@
-const a = 123;
-console.log("test message! " + a);
+const slides = document.querySelectorAll('.slides > .slide');
+
+const timeout = 7000;
+
+let slide = startFromSlide();
+
+start();
+
+function start() {
+    setTimeout(run, timeout);
+}
+
+function run() {
+    slider();
+    setTimeout(run, timeout);
+}
+
+function slider() {
+    increment();
+    changeSlide();
+}
+
+function changeSlide() {
+    slides[slide].classList.add('show');
+
+    for (let i = 0; i < slides.length; i++) {
+        if (i != slide) {
+            slides[i].classList.remove('show');
+        }
+    }
+}
+
+function increment() {
+    slide++;
+    if (slide > slides.length - 1) {
+        slide = 0;
+    }
+}
+
+function startFromSlide() {
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].classList.contains('show')) {
+            return i;
+        }
+    }
+}
